8382f8e652cf081256865e1dd7a2b7a29c91374d
Nik <125446403+Nikita3271@users.noreply.github.com>
2024-05-13 11:26:39 +0300

    fix

--- DjangoWebProject1/settings.py
+++ DjangoWebProject1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7604cf91-7cb4-4d09-a70a-26c520e8aa4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
